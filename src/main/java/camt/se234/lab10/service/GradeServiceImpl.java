c3b4ddd83470887c7f29a070845561e578be9bab
unknown <donutzue@gmail.com>
2018-03-30 00:15:43 +0700

    fix testcases

--- src/main/java/camt/se234/lab10/service/GradeServiceImpl.java
+++ src/main/java/camt/se234/lab10/service/GradeServiceImpl.java
@@ -6,8 +6,21 @@ import org.springframework.stereotype.Service;
 @Service
 public class GradeServiceImpl implements GradeService {
     @Override
-    public String getGrade(double score) {  
+    public String getGrade(double score) {
+        if(score>=80&&score<=100){
+            return "A";
+        }
+        else if(score>=75){
+            return "B";
+        }
+        else if(score>=60){
+            return "C";
+        }
+        else if(score>=33){
+            return "D";
+        }
+        else {
             return "F";
-
+        }
     }
 }
