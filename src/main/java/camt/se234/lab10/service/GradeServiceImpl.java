6c44214155b656d378de13c4037d489649a068b6
Unknown <donutzue@gmail.com>
2018-03-30 16:07:46 +0700

    Implement the GradeServiceImpl
    implement the method to past the TDD

--- src/main/java/camt/se234/lab10/service/GradeServiceImpl.java
+++ src/main/java/camt/se234/lab10/service/GradeServiceImpl.java
@@ -25,23 +25,23 @@ public class GradeServiceImpl implements GradeService {
     }
     @Override
     public String getGrade(double midtermScore, double finalScore) {
-//        double score=(midtermScore+finalScore)/2;
-//        if(score>=80&&score<=100){
-//            return "A";
-//        }
-//        else if(score>=75){
-//            return "B";
-//        }
-//        else if(score>=60){
-//            return "C";
-//        }
-//        else if(score>=33){
-//            return "D";
-//        }
-//        else {
-//            return "F";
-//        }
-        return null;
+        double score=(midtermScore+finalScore)/2;
+        if(score>=80&&score<=100){
+            return "A";
+        }
+        else if(score>=75){
+            return "B";
+        }
+        else if(score>=60){
+            return "C";
+        }
+        else if(score>=33){
+            return "D";
+        }
+        else {
+            return "F";
+        }
+
 
     }
 }
